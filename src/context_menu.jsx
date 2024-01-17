daeb8d9d0684e1c51faa2c14803bf4ba1f398b7b
BuildTools <mariothrowsfieball@gmail.com>
2024-01-17 07:42:12 +0100

    added context menu

---
+++ src/context_menu.jsx
@@ -0,0 +1,14 @@
+import './app.css'
+
+
+export function ContextMenu() {
+  return (
+    <ul class="context_menu">
+      <li>Add Note</li>
+      <li>Create Anki Card</li>
+      <li>X-Ray(not yet implemented)</li>
+      <li>Definition(not yet implemented)</li>
+      <li>Translate(not yet implemented)</li>
+    </ul>
+  )
+}
---
+++ src/utils/get_position.js
@@ -0,0 +1,21 @@
+export function getPosition(e) {
+  var posx = 0;
+  var posy = 0;
+
+  if (e.pageX || e.pageY) {
+    posx = e.pageX;
+    posy = e.pageY;
+  } else if (e.clientX || e.clientY) {
+    posx =
+      e.clientX +
+      document.body.scrollLeft +
+      document.documentElement.scrollLeft;
+    posy =
+      e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+  }
+
+  return {
+    x: posx,
+    y: posy
+  };
+}
