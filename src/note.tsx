434c0912e6343a2e3d0b2bf4ad01ce121807df8a
BuildTools <mariothrowsfieball@gmail.com>
2024-01-20 16:17:04 +0100

    addd ability to save notes and choose location

--- src/note.tsx
+++ src/note.tsx
@@ -1,5 +1,8 @@
 import { StateUpdater, useState, useEffect } from "preact/hooks"
 import { createRef } from "preact"
+import { BaseDirectory, writeTextFile } from "@tauri-apps/api/fs"
+import { open } from "@tauri-apps/api/dialog"
+import { signal } from "@preact/signals"
 import EasyMDE from "easymde"
 import "./app.css"
 import "../node_modules/easymde/dist/easymde.min.css"
@@ -10,9 +13,13 @@ type NoteProps = {
 	setIsEditorOpen: StateUpdater<boolean>
 }
 
+const name = signal(`${Date.now()}.md`)
+const path = signal<BaseDirectory | string>(BaseDirectory.Home)
+
 export function Note({ content, isOpen, setIsEditorOpen }: NoteProps) {
 	const editor = createRef()
 	const dialog = createRef()
+	const error = signal("")
 	const [easyMDE, setEasyMDE] = useState(undefined)
 
 	useEffect(() => {
@@ -37,10 +44,53 @@ export function Note({ content, isOpen, setIsEditorOpen }: NoteProps) {
 		}
 	}, [isOpen, dialog])
 
+	async function saveFile() {
+		try {
+			await writeTextFile(`${path.value}/${name.value}`, easyMDE.value())
+		} catch (err) {
+			error.value = `error: ${err}`
+		}
+	}
+
+	function updateName(e: InputEvent) {
+		if (!(e.currentTarget instanceof HTMLInputElement)) {
+			return
+		}
+		name.value = e.currentTarget.value
+	}
+
+	function updatePath(e: InputEvent) {
+		if (!(e.currentTarget instanceof HTMLInputElement)) {
+			return
+		}
+		name.value = e.currentTarget.value
+	}
+
+	async function openDir() {
+		try {
+			const directory = await open({
+				multiple: false,
+				directory: true,
+			})
+			if (Array.isArray(directory)) {
+				return
+			}
+			path.value = directory
+		} catch (err) {
+			error.value = err
+		}
+	}
+
 	return (
 		<dialog class="editor" ref={dialog}>
+			<p>{error.value}</p>
+			<p>File Name</p>
+			<input onInput={updateName} value={name.value} />
+			<p>Path</p>
+			<input onInput={updatePath} value={path.value} />
+			<button onClick={openDir}>Chose Folder</button>
 			<button onClick={() => setIsEditorOpen(false)}>Close</button>
-			<button>Save</button>
+			<button onClick={saveFile}>Save</button>
 			<textarea ref={editor} />
 		</dialog>
 	)
